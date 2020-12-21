771b0e748b23381cd4fd3f621ed9991016884a21
Netfather <ahhssxlcwjz@163.com>
2020-12-21 17:48:16 +0900

    FashionMnist Learning

--- Tensorflow2.0xLearning/ch6/ch6_1_FahsionMnist.py
+++ Tensorflow2.0xLearning/ch6/ch6_1_FahsionMnist.py
@@ -28,8 +28,8 @@ batchsize = 128
 db = tf.data.Dataset.from_tensor_slices((x,y))
 db = db.batch(batchsize).shuffle(10000)
 
-x_test = tf.convert_to_tensor(x,dtype = tf.float32) / 255.
-y_test = tf.convert_to_tensor(y,dtype = tf.int32)
+x_test = tf.convert_to_tensor(x_test,dtype = tf.float32) / 255.
+y_test = tf.convert_to_tensor(y_test,dtype = tf.int32)
 db_test = tf.data.Dataset.from_tensor_slices((x_test,y_test))
 db_test = db_test.batch(batchsize)
     
@@ -44,15 +44,46 @@ model = Sequential([
     layers.Dense(10,activation = tf.nn.relu)
     ])
 
+model.build(input_shape = [None,28*28])
+optimizer = optimizers.Adam(lr = 1e-4)
+#model.summary()
+#%%
 
+for epoch in range(5):
+    
+    for step, (x,y) in enumerate(db):
+        x = tf.reshape(x,[-1,28*28])
+        
+        with tf.GradientTape() as tape:
+            #[num,784] -> [num,10]
+            logits = model(x)
+            y_onehot = tf.one_hot(y,depth = 10)
+            # loss = tf.reduce_mean(tf.losses.MSE(y_onehot,logits))
+            loss2 = tf.reduce_mean(tf.losses.categorical_crossentropy(y_onehot,logits,from_logits = True))
+        grads = tape.gradient(loss2,model.trainable_variables)
+        optimizer.apply_gradients(zip(grads,model.trainable_variables))      
 
+        if step % 100 == 0:
+            print(epoch,step,'loss:',float(loss2))
 
-
-
-
-
-
-
+        #Test
+    total_correct =0
+    total_num = 0
+    for x,y in db_test:
+        x = tf.reshape(x,[-1,28*28])
+        logits = model(x)
+        
+        prob = tf.nn.softmax(logits, axis = 1)
+        pred = tf.argmax(prob,axis = 1)
+        pred = tf.cast(pred,dtype = tf.int32)
+        correct = tf.equal(pred,y)
+        correct = tf.reduce_sum(tf.cast(correct,dtype = tf.int32))
+        total_correct += int(correct)
+        total_num += x.shape[0]
+    print(total_num)
+    acc = total_correct / total_num
+    print('acc:' , acc)
+            
 
 
 
